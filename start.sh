e62ceb1052b84cfdc047471c99c5e54fa132876b
cmelgarejo <cmelgarejo.dev@gmail.com>
2020-04-03 14:46:58 -0400

    feat: add more configs to env variables

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 source .env
+
+sed -i "s/\[DATABASE_RDBMS\]/$DATABASE_RDBMS/g" traccar.xml
 sed -i "s/\[DATABASE_DRIVER\]/$DATABASE_DRIVER/g" traccar.xml
-sed -i "s/\[DATABASE_URL\]/$DATABASE_URL/g" traccar.xml
+sed -i "s/\[DATABASE_OPTIONS\]/$DATABASE_OPTIONS/g" traccar.xml
+sed -i "s/\[DATABASE_HOST\]/$DATABASE_HOST/g" traccar.xml
+sed -i "s/\[DATABASE_PORT\]/$DATABASE_PORT/g" traccar.xml
 sed -i "s/\[DATABASE_USER\]/$DATABASE_USER/g" traccar.xml
 sed -i "s/\[DATABASE_PASSWORD\]/$DATABASE_PASSWORD/g" traccar.xml
 
