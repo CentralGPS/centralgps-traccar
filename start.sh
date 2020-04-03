41551d469fef5fa63eefd40dd2ca689877fdf1e7
cmelgarejo <cmelgarejo.dev@gmail.com>
2020-04-03 14:48:48 -0400

    fix: missing DATABASE on start server

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source .env
 
+sed -i "s/\[DATABASE\]/$DATABASE/g" traccar.xml
 sed -i "s/\[DATABASE_RDBMS\]/$DATABASE_RDBMS/g" traccar.xml
 sed -i "s/\[DATABASE_DRIVER\]/$DATABASE_DRIVER/g" traccar.xml
 sed -i "s/\[DATABASE_OPTIONS\]/$DATABASE_OPTIONS/g" traccar.xml
